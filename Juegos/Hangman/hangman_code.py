be65e3e5d3d5ed6fd9fe02417ff69b7ab2802dd4
Javier Castillo <javiercastillou@gmail.com>
2025-09-23 12:22:05 +0200

    Translation Juegos to English

--- Juegos/Hangman/hangman_code.py
+++ Juegos/Hangman/hangman_code.py
@@ -62,34 +62,34 @@ def hangman(failures):
 
 def guessed_letter() -> str:
       while True:
-            guessed_letter = str(input("Escoge una letra: "))
+            guessed_letter = str(input("Choose a letter: "))
       
             if len(guessed_letter) == 1 and guessed_letter.isalpha():
                   break
-            print("¡Una sola letra!")
+            print("Just one letter!")
       return guessed_letter
 
-def quitar_acentos(texto):
-    texto_sin_acentos = ''.join((c for c in unicodedata.normalize('NFD', texto) if unicodedata.category(c) != 'Mn'))
-    return texto_sin_acentos
+def remove_especial_characters(texto):
+    plain_text = ''.join((c for c in unicodedata.normalize('NFD', texto) if unicodedata.category(c) != 'Mn'))
+    return plain_text
 
-def jugada(word, failed_letters, coded_word, failures):
-      posiciones = []
-      guess_letter = quitar_acentos(guessed_letter()).upper()
-      # Usar un bucle for para buscar todas las ocurrencias de la letra
+def game_play(word, failed_letters, coded_word, failures):
+      positions = []
+      guess_letter = remove_especial_characters(guessed_letter()).upper()
+      # Use a loop to find every letter
 
       if (guess_letter in failed_letters) or (guess_letter+" " in coded_word):
             error = 100
             failures += 1
       else:
             for i in range(len(word)):
-                  if quitar_acentos(word[i]) == guess_letter:
-                        posiciones.append(i)
+                  if remove_especial_characters(word[i]) == guess_letter:
+                        positions.append(i)
 
-      # Comprobar si la letra está en la palabra y mostrar las posiciones
-            if posiciones:
+      # Check if the word contains the letter and show the positions
+            if positions:
                   error = 101
-                  for i in posiciones: coded_word[i] = word[i]+" "
+                  for i in positions: coded_word[i] = word[i]+" "
             else:
                   failures += 1
                   failed_letters.append(guess_letter)
@@ -97,28 +97,26 @@ def jugada(word, failed_letters, coded_word, failures):
 
       return failed_letters, coded_word, failures, error, guess_letter
 
-def corregir_ortografia(texto, lang):
-    # Crear una instancia del corrector ortográfico para el idioma especificado
+def ortography_corrector(texto, lang):
+    # Create the corrector instance for a given language
     spell = SpellChecker(language=lang)
 
-    # Dividir el texto en palabras
-    palabras = texto.split()
+    words = texto.split()
 
-    # Encontrar y corregir palabras mal escritas
-    correcciones = []
-    for palabra in palabras:
-        # Obtener la corrección sugerida para la palabra
-        correccion = spell.correction(palabra)
-        correcciones.append(correccion)
+    # Find and correct spelling errors
+    corrections = []
+    for word in words:
+        # Suggested correction
+        correction = spell.correction(word)
+        corrections.append(correction)
 
-    # Unir las palabras corregidas en un solo string
-    texto_corregido = ' '.join(correcciones)
+    corrected_text = ' '.join(corrections)
 
-    return texto_corregido.upper()
+    return corrected_text.upper()
 
 
 system("clear")
-print("Tu palabra no puede tener caracteres especiales como ., !, ?, etc")
+print("Your word cannot include special characters like: ., !, ?, etc")
 
 lang = {"English": "en",
         "Español": "es",
@@ -126,13 +124,13 @@ lang = {"English": "en",
         "Português": "pt",
         "Deutsch": "de"}
 
-word = input("Escoge la palabra a adivinar: ")
-word = corregir_ortografia(word, lang["Español"])
+word = input("Choose a word to find: ")
+word = ortography_corrector(word, lang["English"])
 total_characters = len(word)
 system("clear")
 
 coded_word = []
-print("El juego empieza: ")
+print("The game begins: ")
 for i in range(total_characters): 
     if word[i] == " ":
         coded_word.append("  ")
@@ -143,41 +141,41 @@ final_word = ''.join(coded_word)
 failed_letters = []
 failures=0
 hangman(failures)
-print("La palabra no contiene las siguientes letras: ", sorted(failed_letters))
+print("The word doesn't contains the following letters: ", sorted(failed_letters))
 print("")
-print("La actual palabra: " + final_word)
+print("The actual word: " + final_word)
 print("")
 while (failures < 6):
-    failed_letters, coded_word, failures, error, guess_letter = jugada(word, failed_letters, coded_word, failures)
+    failed_letters, coded_word, failures, error, guess_letter = game_play(word, failed_letters, coded_word, failures)
     final_word = ''.join(coded_word)
 
     system("clear")
     if error == 100:
-        print("La letra " + guess_letter + " ya ha sido usada :/")
+        print("The " + guess_letter + " letter has been used :/")
     elif error == 101:
-        print("¡La letra " + guess_letter + " está en la palabra!")
+        print("The word includes the " + guess_letter + " letter!")
     elif error == 102:
-        print("La letra " + guess_letter + " NO está en la palabra...")
+        print("The word does NOT include the " + guess_letter + " letter...")
     hangman(failures)
-    print("La palabra no contiene las siguientes letras: ", sorted(failed_letters))
+    print("The word doesn't contains the following letters: ", sorted(failed_letters))
     print("")
-    print("La actual palabra: " + final_word)
+    print("The actual word: " + final_word)
     print("")
     if "_ " not in coded_word:
         break
 
 system("clear")
 if failures < 6:
-    print("Ganaste!")
-    print("La palabra era: "+final_word)
+    print("You win!")
+    print("The word is: "+final_word)
     print("")
 else:
     hangman(failures)
     print("")
-    print("Perdiste :(")
+    print("Game over :(")
     coded_word = []
     for i in range(total_characters): coded_word.append(word[i]+" ")
     final_word = ''.join(coded_word)
-    print("La palabra era: "+final_word)
+    print("The word was: "+final_word)
     print("")
 
